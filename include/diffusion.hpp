6248cbf5a77af97e5e92f1d90f98a251ce344dad
Khanh Ma <baokhanhma2007@gmail.com>
2026-04-20 23:09:08 -0400

    Doc

---
+++ include/diffusion.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+
+class Mesh;
+
+std::vector<double> run_diffusion_case(const Mesh& mesh);
--- include/mesh.hpp
+++ include/mesh.hpp
@@ -13,8 +13,10 @@ struct Vec3{
 
 class Mesh {
     public:
+        /* Prototype for constructor */
         Mesh(std::size_t nx, std::size_t ny, std::size_t nz,
                 double lx, double ly, double lz);
+
         std::size_t nx() const { return nx_; }
         std::size_t ny() const { return ny_; }
         std::size_t nz() const { return nz_; }
@@ -33,15 +35,25 @@ class Mesh {
 
         Vec3 cell_center(std::size_t c) const;
 
+        bool on_xmin(std::size_t i) const { return i == 0; }
+        bool on_xmax(std::size_t i) const { return i + 1 == nx_; }
+        bool on_ymin(std::size_t j) const { return j == 0; }
+        bool on_ymax(std::size_t j) const { return j + 1 == ny_; }
+        bool on_zmin(std::size_t k) const { return k == 0; }
+        bool on_zmax(std::size_t k) const { return k + 1 == nz_; }
+
     private:
+        /* How many cells in each dimension? */
         std::size_t nx_{};
         std::size_t ny_{};
         std::size_t nz_{};
 
+        /* What is the physical size in each dimension? */
         double lx_{};
         double ly_{};
         double lz_{};
 
+        /* What is the size of each cell in each dimension? */
         double dx_{};
         double dy_{};
         double dz_{};
